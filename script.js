aed695275a3512828054865278fccbd12518b3a8
halizha26 <dudel2603@gmail.com>
2024-06-02 15:38:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript sederhana untuk mengirim formulir kontak
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Pesan Anda telah dikirim!');
+});
